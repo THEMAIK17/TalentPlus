e883bd57d53f7f9451c2d180bb7297ffa2cca9df
armando maikol duran salas <maikold774@gmail.com>
2025-12-10 11:21:59 -0500

    feat: the connection to database was created

---
+++ TalentPlus.Domain/Entities/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace TalentPlus.Domain.Entities;
+
+public class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    // Relationship: One department has many employees
+    public ICollection<Employee>? Employees { get; set; }
+}
